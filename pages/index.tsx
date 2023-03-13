590f4974f255815e54b9976d077bf1b116e734ee
sudachi <takomannto1996@gmail.com>
2023-03-13 11:01:13 +0900

    必要なライブラリのインストール

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,34 @@
-import Head from 'next/head'
-import tw from 'twin.macro'
-import { Button, Logo } from '../components'
-
-const styles = {
-  // Move long class sets out of jsx to keep it scannable
-  container: ({ hasBackground }: { hasBackground: boolean }) => [
-    tw`flex flex-col items-center justify-center h-screen`,
-    hasBackground && tw`bg-gradient-to-b from-electric to-ribbon`,
-  ],
-}
+import { motion } from 'framer-motion'
+import 'twin.macro'
+import { css } from 'twin.macro'
 
 const App = () => (
-  <div css={styles.container({ hasBackground: true })}>
-    <Head>
-      <title>_</title>
-    </Head>
-    <div tw="flex flex-col justify-center h-full gap-y-5">
-      <Button variant="primary">Submit</Button>
-      <Button variant="secondary">Cancel</Button>
-      <Button isSmall>Close</Button>
-    </div>
-    <Logo />
+  <div>
+    <motion.div
+      tw="absolute w-96 h-96 bg-yellow-300"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+    />
+    <motion.div
+      tw="absolute left-96"
+      css={[
+        css`
+          width: 0;
+          height: 0;
+          border-left: 100px solid transparent;
+          border-right: 100px solid transparent;
+          border-bottom: 384px solid rgb(153, 27, 27);
+        `,
+      ]}
+      initial={{ rotate: 0 }}
+      // whileInView={{ rotate: [0, -120, -80, -90] }}
+      animate={{
+        x: [0, 0, 0, 0, 0, 0, -200],
+        rotate: [0, -140, -60, -110, -80, -90, -90],
+      }}
+      transition={{ duration: 1.5, times: [0, 0.25, 0.5, 0.7, 0.9, 1] }}
+    />
+    <div tw="h-[1800px] bg-gray-200"></div>
   </div>
 )
 

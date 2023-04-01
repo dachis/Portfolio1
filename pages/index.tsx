45f7c87146f3b58ae1497c853635ec49fc03e8b3
sudachi <takomannto1996@gmail.com>
2023-04-01 10:29:10 +0900

    useTransformの実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import 'twin.macro'
 import { css } from 'twin.macro'
 import Top from '../components/Top'
+import {
+  useMotionValue,
+  useMotionValueEvent,
+  motion,
+  useTransform,
+} from 'framer-motion'
 
 const App = () => {
   const [playAnimation, setPlayAnimation] = useState(false)
+  const ref = useRef<HTMLDivElement>(null)
+  const x = useMotionValue(0)
+  const backgroundColor = useTransform(
+    x,
+    [0, 1000, 2000],
+    ['rgb(10, 10, 10)', 'rgb(240, 240, 240)', 'rgb(240, 20, 240)'],
+  )
+
+  useMotionValueEvent(x, 'animationStart', () => {
+    console.log('animation started on x')
+  })
+
+  useMotionValueEvent(x, 'change', latest => {
+    console.log('x changed to', latest)
+  })
 
   const handleClick = () => {
     setPlayAnimation(!playAnimation)
@@ -23,7 +44,14 @@ const App = () => {
         </div>
         <Top playAnimation={playAnimation} />
       </div>
-      <div tw="h-screen"></div>
+      <div ref={ref} tw="h-screen">
+        <motion.div
+          tw="w-52 h-52"
+          style={{ x, backgroundColor }}
+          drag="x"
+          dragConstraints={ref}
+        />
+      </div>
     </>
   )
 }
